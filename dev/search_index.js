495306778fe39de1dd00785f003d99dc2ea1a1f5
Documenter.jl <documenter@juliadocs.github.io>
2023-11-11 08:26:26 +0000

    build based on aac927d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = jp1","category":"page"},{"location":"#jp1","page":"Home","title":"jp1","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for jp1.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [jp1]","category":"page"}]
+}
